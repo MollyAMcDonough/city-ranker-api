954d0a0458dafae6c94be5d250844f5625f78fee
Molly Ann McDonough <mcdonough.mollya@gmail.com>
2022-06-15 18:45:27 -0400

    commented out byebug require

--- app/serializers/city_serializer.rb
+++ app/serializers/city_serializer.rb
@@ -1,4 +1,4 @@
-require 'byebug'
+# require 'byebug'
 class CitySerializer < ActiveModel::Serializer
   attributes :id, :city, :latitude, :longitude, :elevation_meters, :monthly_after_tax_salary, :apt_price_per_meter2_outside_city_center, :apt_price_per_meter2_inside_city_center, :monthly_rent_one_bdrm_outside_city_center, :monthly_rent_one_bdrm_inside_city_center, :monthly_utilities, :monthly_internet, :population, :on_water, :coastal, :airport, :distance
   has_one :region
